aa1ee09d675b4c7509466ca155cf5fbd218f8061
ravi-rana <ranaravishanker@gmail.com>
2016-09-20 13:56:27 +0530

    my first switch program in c

---
+++ switch.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	switch(a)
+	{
+		case 1:printf("you are looser\n");
+		case 2:printf("you are awseome\n");
+		case 3:printf("you are  garbage\n");
+		break;
+		case 0:printf("you are unique\n");
+		break;
+		default:printf("i don't no\n");
+	}
+	return 0;
+}
+
+		
+                  
+		
+
+
